0fbf6f902eb5298b15c90a9a85260f370f02b0fc
Chiranjib Roy <137825243+cobra1x@users.noreply.github.com>
2025-11-30 19:31:06 +0530

    Change CORS settings to allow specific origin
    Updated CORS middleware to allow specific origins.

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import router
 
 app= FastAPI()
+source=[
+    "https://ai-legalmate.vercel.app/"
+]
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=source,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 
 )
+
 
 app.include_router(router)
